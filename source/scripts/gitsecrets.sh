43b094b155708aa893740ec7bdaeb410247793a1
adrazhi <adrazhi@broadinstitute.org>
2019-04-17 11:57:23 -0400

    add git secrets installation script

--- source/scripts/gitsecrets.sh
+++ source/scripts/gitsecrets.sh
@@ -1,11 +1,41 @@
 #!/usr/bin/env bash
 
-# Install git-secrets package
-brew install git-secrets
+install_homebrew_if_not_present() {
+    echo "Checking for homebrew installation"
+    which -s brew
+    if [[ $? != 0 ]] ; then
+        echo "Homebrew not found. Installing..."
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+        echo "Homebrew already installed!"
+    fi
+}
 
-# Add a configuration template if you want to add hooks to all repositories you initialize or clone in the future.
-git secrets --register-aws --global
+setup_git_secrets() {
+    # Install git-secrets
+    brew install git-secrets
 
-# Add hooks to all your local repositories.
-git secrets --install ~/.git-templates/git-secrets
-git config --global init.templateDir ~/.git-templates/git-secrets
+    # Enable on all repos
+    git secrets --install ~/.git-secrets -f
+    git config --global core.hooksPath ~/.git-secrets/hooks -f
+
+    # For AWS; git secrets has a function just to do that
+    git secrets --register-aws --global
+    # Regex 16-digit-string wrapped with double quotes
+    git secrets --add --global '"[A-Za-z0-9/\+=]{16}"'
+    # Literal 'private_key'
+    git secrets --add --global 'private_key'
+    git secrets --add --global 'private_key_id'
+    # Literal 'client_email'; gcp related
+    git secrets --add --global --literal 'client_email'
+    # Literal 'client_id'; gcp related
+    git secrets --add --global --literal 'client_id'
+    # Literal 'notification_token'
+    git secrets --add --global --literal 'notification_token'
+    # Literal 'cromwell_password'
+    git secrets --add --global --literal 'cromwell_password'
+}
+
+
+install_homebrew_if_not_present
+setup_git_secrets
